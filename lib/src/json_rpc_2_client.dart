6c209cb9a3ca96cf9d82e67e9a776dab1ce5ddc0
Marco Bramini <braminim@gmail.com>
2018-10-30 11:35:42 +0100

    Add JsonRpc2Client implementation with tests

---
+++ lib/src/json_rpc_2_client.dart
@@ -0,0 +1,144 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+// JsonRpc2Client is a JSON-RPC 2.0 standard compliant client.
+// It is compatible with both Web and Flutter applications,
+// using BrowserClient or Client, respectively, to initialize it.
+//
+// In case of rpc related error, it returns a JsonRpcException.
+//
+// Example:
+//  _exampleRpcClient = new JsonRpc2Client(httpClient, _exampleURL);
+//  _exampleRpcClient.sendRequest("exampleMethod", params: {"exampleParamKey": exampleParamValue})
+//    .then((result) => doSomethingWithResult());
+//
+class JsonRpc2Client {
+  final http.Client _httpClient;
+  final String _serverURL;
+
+  Map<String, String> _requestHeaders = {'Content-Type': 'application/json'};
+
+  // Sets an header which will be sent with the next requests.
+  setRequestHeader(String name, String value) {
+    _requestHeaders[name] = value;
+  }
+
+  JsonRpc2Client(this._httpClient, this._serverURL);
+
+  // Sends a JSON-RPC 2.0 Request object, using given method and params, if present.
+  // A JSON-RPC 2.0 Response is a json containing:
+  //  - id: A numeric identifier established by the Client, which should not be null.
+  //  - jsonrpc: A String specifying the version of the JSON-RPC protocol.
+  //  - method: A String containing the name of the method to be invoked.
+  //  - params: A Structured value that holds the parameter values to be used
+  //    during the invocation of the method.
+  //
+  // In case of error, returns a JsonRpcError.
+  Future sendRequest(String method,
+      {Map<String, Object> params: const {}}) async {
+    Map<String, Object> request = {
+      'id': 1,
+      'jsonrpc': '2.0',
+      'method': method,
+      'params': params,
+    };
+
+    final http.Response response = await _executeHttpPostRequest(request);
+
+    // A different status code means server/http error, not a json rpc error
+    if (response.statusCode != 200) {
+      throw HttpException(response.statusCode, response.reasonPhrase);
+    }
+
+    var bodyMap;
+    try {
+      bodyMap = json.decode(response.body);
+    } catch (formatException) {
+      throw formatException;
+    }
+
+    if (!_isValidJsonRpcResponse(bodyMap)) {
+      throw FormatException("invalid JSON-RPC 2.0 response received");
+    }
+
+    if (bodyMap.containsKey('error')) {
+      var errorMap = bodyMap['error'];
+
+      throw JsonRpcError(
+        errorMap['code'],
+        errorMap['message'],
+        (errorMap.containsKey('data') ? errorMap['data'] : null),
+      );
+    }
+
+    return bodyMap['result'];
+  }
+
+  // Sends a JSON-RPC 2.0 Notification object, using given method and params, if present.
+  // A JSON-RPC 2.0 Notification is a Response without the 'id' field.
+  // No error or response is returned.
+  void sendNotification(String method, {Map<String, Object> params: const {}}) async {
+    Map<String, Object> notification = {
+      'jsonrpc': '2.0',
+      'method': method,
+      'params': params,
+    };
+
+    final http.Response response = await _executeHttpPostRequest(notification);
+
+    // A different status code means server/http error, not a json rpc error
+    if (response.statusCode != 200) {
+      throw HttpException(response.statusCode, response.reasonPhrase);
+    }
+  }
+
+  // Validates if the received response matches the JSON-RPC 2.0 standard.
+  // The response should be decoded before validation.
+  bool _isValidJsonRpcResponse(Map<String, dynamic> response) {
+    if (response.containsKey('jsonrpc') &&
+        (response.containsKey('result') ||
+            response.containsKey('error') &&
+                response['error'].containsKey('code') &&
+                response['error'].containsKey('message')) &&
+        response.containsKey('id')) {
+      return true;
+    }
+
+    return false;
+  }
+
+  Future<http.Response> _executeHttpPostRequest(Map<String, Object> payload) {
+    return _httpClient.post(
+      _serverURL,
+      headers: _requestHeaders,
+      body: json.encode(payload),
+    );
+  }
+}
+
+// JsonRpcError is the representation of a JSON-RPC 2.0 Error.
+// It contains a code which identifies the error, a message which describes it and,
+// if present, data which contains additional information.
+class JsonRpcError implements Exception {
+  int code;
+  String message;
+  Object data;
+
+  JsonRpcError(this.code, this.message, [this.data]);
+
+  toString() => "jsonrpc error: $code: '$message'";
+}
+
+// HttpException is the representation of an error received when
+// something goes wrong with the response of an HTTP request.
+// It contains the response status code and the reason phrase describing the error.
+class HttpException implements Exception {
+  int statusCode;
+  String reasonPhrase;
+
+  HttpException(this.statusCode, this.reasonPhrase);
+
+  toString() => "transport error: $statusCode $reasonPhrase";
+}
--- test/json_rpc_2_client_test.dart
+++ test/json_rpc_2_client_test.dart
@@ -1,16 +1,165 @@
-import 'package:json_rpc_2_client/json_rpc_2_client.dart';
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
+import 'package:json_rpc_2_client/src/json_rpc_2_client.dart';
+
+class MockClient extends Mock implements Client {}
 
 void main() {
-  group('A group of tests', () {
-    Awesome awesome;
+  const String mockMethod = "mock-method";
+  const String mockServerURL = "https://mock-url.net/";
+  const Map<String, Object> mockParamsMap = {"mockKey": "mockValue"};
+  const Map<String, String> mockHeadersMap = {
+    'Content-Type': 'application/json'
+  };
 
-    setUp(() {
-      awesome = new Awesome();
+  String validResponseJson() =>
+      '{"jsonrpc":"2.0","result":[{"ID":"5baa67721752be0001669f73","createdAtISO8601":"2018-09-25T16:50:58.064Z","officeID":"QQST000000051"}],"id":1}';
+  String malformedResponseJson() =>
+      '{"jsonrpc":"2.0result":[{"ID":"5baa67721752be0001669f73","createdAtISO8601":"2018-09-25T16:50:58.064Z","officeID":"QQST000000051"}],"id":1}';
+  String erroredResponseJson() =>
+      '{"jsonrpc":"2.0","error":{"code":-32000,"message":"Server error"},"id":1}';
+  String invalidJsonRpc2ResponseJson() =>
+      '{"jso3nrpc":"2.0","erdror":{"code":-32000,"message":"Server error"},"id":1}';
+
+  Map<String, Object> validRequestObjectMap() => {
+        "id": 1,
+        "jsonrpc": "2.0",
+        "method": "mock-method",
+        "params": {"mockKey": "mockValue"},
+      };
+  Map<String, Object> validNotificationObjectMap() => {
+        "jsonrpc": "2.0",
+        "method": "mock-method",
+        "params": {"mockKey": "mockValue"},
+      };
+
+  final Client httpClient = MockClient();
+  JsonRpc2Client sut;
+
+  setUp(() {
+    sut = JsonRpc2Client(httpClient, mockServerURL);
+  });
+
+  group('JsonRpc2Client', () {
+    test('Test that JsonRpc2Client uses headers set manually', () async {
+      when(httpClient.post(any,
+              headers: anyNamed("headers"), body: anyNamed("body")))
+          .thenAnswer((_) async => Response(validResponseJson(), 200));
+
+      sut.setRequestHeader("newHeader", "newHeaderValue");
+
+      await sut.sendRequest(mockMethod, params: mockParamsMap);
+
+      Map<String, String> expectedHeaders = new Map<String, String>();
+      expectedHeaders..addAll(mockHeadersMap);
+      expectedHeaders["newHeader"] = "newHeaderValue";
+
+      verify(httpClient.post(mockServerURL,
+          headers: expectedHeaders,
+          body: json.encode(validRequestObjectMap())));
     });
 
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
+    test('Test that JsonRpc2Client sends a valid notification object', () async {
+      when(httpClient.post(any,
+              headers: anyNamed("headers"), body: anyNamed("body")))
+          .thenAnswer((_) async => Response(validResponseJson(), 200));
+
+      await sut.sendNotification(mockMethod, params: mockParamsMap);
+
+      verify(httpClient.post(mockServerURL,
+          headers: mockHeadersMap,
+          body: json.encode(validNotificationObjectMap())));
+    });
+
+    test('Test that JsonRpc2Client sends a valid request object', () async {
+      when(httpClient.post(any,
+              headers: anyNamed("headers"), body: anyNamed("body")))
+          .thenAnswer((_) async => Response(validResponseJson(), 200));
+
+      await sut.sendRequest(mockMethod, params: mockParamsMap);
+
+      verify(httpClient.post(mockServerURL,
+          headers: mockHeadersMap, body: json.encode(validRequestObjectMap())));
+    });
+
+    test(
+        'Test that JsonRpc2Client returns a valid result when a valid response is received from server',
+        () async {
+      when(httpClient.post(any,
+              headers: anyNamed("headers"), body: anyNamed("body")))
+          .thenAnswer((_) async => Response(validResponseJson(), 200));
+
+      var responseJson =
+          await sut.sendRequest(mockMethod, params: mockParamsMap);
+
+      var expectedResult = [
+        {
+          "ID": "5baa67721752be0001669f73",
+          "createdAtISO8601": "2018-09-25T16:50:58.064Z",
+          "officeID": "QQST000000051"
+        }
+      ];
+
+      expect(responseJson, equals(expectedResult));
+    });
+
+    test(
+        'Test that JsonRpc2Client throws a FormatException when a malformed response is received',
+        () async {
+      when(httpClient.post(any,
+              headers: anyNamed("headers"), body: anyNamed("body")))
+          .thenAnswer((_) async => Response(malformedResponseJson(), 200));
+
+      expect(() => sut.sendRequest(mockMethod, params: mockParamsMap),
+          throwsFormatException);
+    });
+
+    test(
+        'Test that JsonRpc2Client throws a FormatException when an invalid JSON-RPC 2.0 response is received',
+        () async {
+      when(httpClient.post(any,
+              headers: anyNamed("headers"), body: anyNamed("body")))
+          .thenAnswer(
+              (_) async => Response(invalidJsonRpc2ResponseJson(), 200));
+
+      expect(() => sut.sendRequest(mockMethod, params: mockParamsMap),
+          throwsFormatException);
+    });
+
+    test(
+        'Test that JsonRpc2Client throws a HttpException if something goes wrong during http request when sending a Request Object',
+        () async {
+      when(httpClient.post(any,
+              headers: anyNamed("headers"), body: anyNamed("body")))
+          .thenAnswer((_) async => Response(malformedResponseJson(), 500));
+
+      expect(() => sut.sendRequest(mockMethod, params: mockParamsMap),
+          throwsA(TypeMatcher<HttpException>()));
+    });
+
+    test(
+        'Test that JsonRpc2Client throws a HttpException if something goes wrong during http request when sending a Notification Object',
+            () async {
+          when(httpClient.post(any,
+              headers: anyNamed("headers"), body: anyNamed("body")))
+              .thenAnswer((_) async => Response("", 500));
+
+          expect(() => sut.sendNotification(mockMethod, params: mockParamsMap),
+              throwsA(TypeMatcher<HttpException>()));
+        });
+
+    test(
+        'Test that JsonRpc2Client throws a JsonRpcError when an errored jsonrpc response is received',
+        () async {
+      when(httpClient.post(any,
+              headers: anyNamed("headers"), body: anyNamed("body")))
+          .thenAnswer((_) async => Response(erroredResponseJson(), 200));
+
+      expect(() => sut.sendRequest(mockMethod, params: mockParamsMap),
+          throwsA(TypeMatcher<JsonRpcError>()));
     });
   });
 }
